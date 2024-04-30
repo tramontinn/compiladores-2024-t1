a8e645617f8ed511ec234d6b698208c2b6bef1ea
Guilherme Vinicios Tramontin <gvtramontin@gmail.com>
2024-04-29 21:51:47 -0300

    Update lexer

--- src/lexer.py
+++ src/lexer.py
@@ -15,57 +15,131 @@ def lexer():
     """Cria o objeto do analisador léxico."""
     return ply.lex.lex()
 
-tokens = (
-   'NUMBER',
-   'PLUS',
-   'MINUS',
-   'TIMES',
-   'DIVIDE',
-   'LPAREN',
-   'RPAREN',
-)
+reserved = {
+    'program': 'DIR_PROGRAM',
+    'var': 'DIR_VAR',
+    'procedure': 'DIR_PROC',
+    'function': 'DIR_FUNC',
+    'begin': 'DIR_BEGIN',
+    'end': 'DIR_END',
+    'type': 'DIR_TYPE',
+    'of': 'DIR_OF',
+    'const': 'DIR_CONST',
+    'with': 'DIR_WITH',
+    'if': 'STMT_IF',
+    'then': 'STMT_THEN',
+    'else': 'STMT_ELSE',
+    'while': 'STMT_WHILE',
+    'repeat': 'STMT_REPEAT',
+    'for': 'STMT_FOR',
+    'do': 'STMT_DO',
+    'until': 'STMT_UNTIL',
+    'to': 'STMT_TO',
+    'downto': 'STMT_DOWNTO',
+    'case': 'STMT_CASE',
+    'array': 'TYPE_ARRAY',
+    'integer': 'TYPE_INT',
+    'real': 'TYPE_REAL',
+    'character': 'TYPE_CHAR',
+    'boolean': 'TYPE_BOOL',
+    'string': 'TYPE_STRING',
+    'read': 'FN_READ',
+    'readln': 'FN_READLN',
+    'write': 'FN_WRITE',
+    'writeln': 'FN_WRITELN',
+    'nil': 'OP_NIL',
+    'and': 'OP_LOGIC',
+    'div': 'OP_MUL',
+}
 
-# Regular expression rules for simple tokens
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
+tokens = list(reserved.values()) + [
+    'ID',
+    'LIT_INT',
+    'LIT_REAL',
+    'LIT_STRING',
+    'OP_SUM',
+    'OP_MINUS',
+    'OP_MUL',
+    'OP_DIVIDE',
+    'OP_ATRIB',
+    'OP_REL',
+    'OP_LOGIC',
+    'OP_RANGE',
+    'COMMENT',
+    'OP_OPAR',
+    'OP_CPAR',
+    'OP_OBRA',
+    'OP_CBRA',
+    'OP_COMMA',
+    'OP_EOC',
+    'OP_PERIOD',
+    'OP_COLON',
+]
 
-# A regular expression rule with some action code
-def t_NUMBER(t):
+t_ignore = ' \t'
+t_OP_SUM = r'\+'
+t_OP_MINUS = r'-'
+t_OP_MUL = r'\*'
+t_OP_DIVIDE = r'/'
+t_OP_ATRIB = r':='
+t_OP_REL = r'[=<>]=|<>|<=|>=|>|<'
+t_OP_LOGIC = r'and|or|not'
+t_OP_RANGE = r'\.\.'
+t_OP_OPAR = r'\('
+t_OP_CPAR = r'\)'
+t_OP_OBRA = r'\['
+t_OP_CBRA = r'\]'
+t_OP_COMMA = r','
+t_OP_EOC = r';'
+t_OP_PERIOD = r'\.'
+t_OP_COLON = r':'
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+def t_LIT_INT(t):
     r'\d+'
     t.value = int(t.value)
     return t
 
-# Define a rule so we can track line numbers
+def t_LIT_REAL(t):
+    r'\d+\.\d+([eE][-+]?\d+)?'
+    t.value = float(t.value)
+    return t
+
+def t_LIT_STRING(t):
+    r'\'[^\']*\'|\"[^\"]*\"'
+    t.value = t.value[1:-1]
+    return t
+
+def t_COMMENT(t):
+    r'\(\*.*?\*\)|//.*'
+    t.lexer.lineno += t.value.count('\n')
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
-
-# Error handling rule
 def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+    raise Exception("Caractere ilegal '%s' na linha %d, posição %d" % (t.value[0], t.lineno, t.lexpos))
 
-# Build the lexer
 lexer = ply.lex.lex()
 
 data = '''
 3 + 4 * 10
   + -20 *2
+  asdb (12_)
+  ç
+  ~çç
 '''
 
-# Give the lexer some input
 lexer.input(data)
 
-# Tokenize
 while True:
     tok = lexer.token()
     if not tok:
-        break      # No more input
+        break
     print(tok)
+    
